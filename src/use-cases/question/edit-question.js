3c0d779b99a879deba0e7078f683e6b97aa87652
Joe Delamore <hello@joedelamore.com>
2022-03-13 16:44:43 +0100

    feat: added edit-question use case

---
+++ src/use-cases/question/edit-question.js
@@ -0,0 +1,30 @@
+import makeQuestion from '../../entities/question';
+
+export default function makeEditQuestion({ questionsDb }) {
+  return async function editQuestions({ id, ...changes } = {}) {
+    if (!id) {
+      throw new Error('You must supply an id');
+    }
+    if (!changes.body) {
+      throw new Error('You must supply a question');
+    }
+    const existing = await questionsDb.findById({ id });
+
+    if (!existing) {
+      throw new RangeError('Question not foudn');
+    }
+
+    const question = makeQuestion({ ...existing, ...changes, modifiedOn: null });
+    if (question.getHash() === existing.hash) {
+      return existing;
+    }
+    const updated = await questionsDb.update({
+      id: question.getId(),
+      answered: question.getAnswered(),
+      modifiedOn: question.getModifiedOn(),
+      body: question.getBody(),
+      hash: question.getHash(),
+    });
+    return { ...existing, ...updated };
+  };
+}
