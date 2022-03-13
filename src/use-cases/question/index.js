28fd36fcb009c1842486f5ea0adbcb127f595b8c
Joe Delamore <hello@joedelamore.com>
2022-03-13 16:46:26 +0100

    feat: added editQuestion to questionService

--- src/use-cases/question/index.js
+++ src/use-cases/question/index.js
@@ -1,17 +1,22 @@
 import makeAddQuestion from './add-question';
 import makeListQuestions from './list-questions';
+import makeEditQuestion from './edit-question';
 import makeRandomQuestion from './random-question';
 import questionsDb from '../../data-access/questions';
 
 const addQuestion = makeAddQuestion({ questionsDb });
 const listQuestions = makeListQuestions({ questionsDb });
+const editQuestion = makeEditQuestion({ questionsDb });
 const randomQuestion = makeRandomQuestion({ questionsDb });
 
 const questionService = Object.freeze({
   addQuestion,
   listQuestions,
+  editQuestion,
   randomQuestion,
 });
 
 export default questionService;
-export { addQuestion, listQuestions, randomQuestion };
+export {
+  addQuestion, listQuestions, editQuestion, randomQuestion,
+};
