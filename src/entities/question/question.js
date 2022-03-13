df58cb1c05126b192317c356a429853cd8d21bea
Joe <Joe@DESKTOP-M95EUHJ>
2022-03-13 14:13:40 +0100

    feat: added question entity

---
+++ src/entities/question/question.js
@@ -0,0 +1,24 @@
+export default function buildMakeQuestion({ Id, md5 }) {
+  function makeHash(creator, id, body) {
+    return md5(creator + id + body);
+  }
+  return function makeQuestion({
+    creator,
+    createdOn = Date.now(),
+    modifiedOn = Date.now(),
+    id = Id.makeId(),
+    body,
+  } = {}) {
+    if (!Id.isValidId(id)) {
+      throw new Error('Question must have a valid id.');
+    }
+
+    return Object.freeze({
+      getCreator: () => creator,
+      getCreatedOn: () => createdOn,
+      getHash: () => makeHash(creator, id, body),
+      getModifiedOn: () => modifiedOn,
+      getBody: () => body,
+    });
+  };
+}
